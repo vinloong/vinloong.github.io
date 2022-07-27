71471432a1d162b6a490a596864daa2c1591d771
vinloong <21272660+vinloong@users.noreply.github.com>
2022-07-27 22:34:53 +0800

    add all

---
+++ README.md
@@ -0,0 +1,7 @@
+# loong's Website
+
+## Installation
+
+1. `npm install` in the root of the repo (one level above this directory).
+1. In this directory, do `npm start`.
+1. A browser window will open up, pointing to the docs.
---
+++ src/pages/index.tsx
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import clsx from 'clsx';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
+import Link from '@docusaurus/Link';
+import Translate, {translate} from '@docusaurus/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl, {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
+import Layout from '@theme/Layout';
+import Features, {type FeatureItem} from '@site/src/data/features';
+import styles from './styles.module.css';
+
+function HeroBanner() {
+  return (
+    <div className={styles.hero} data-theme="dark">
+      <div className={styles.heroInner}>
+        <h1 className={styles.heroProjectTagline}>
+          <img
+            alt={translate({message: 'Docusaurus with Keytar'})}
+            className={styles.heroLogo}
+            src={useBaseUrl('/img/docusaurus_keytar.svg')}
+            width="200"
+            height="200"
+          />
+          <span
+            className={styles.heroTitleTextHtml}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: translate({
+                id: 'homepage.hero.title',
+                message:
+                  'Build <b>optimized</b> websites <b>quickly</b>, focus on your <b>content</b>',
+                description:
+                  'Home page hero title, can contain simple html tags',
+              }),
+            }}
+          />
+        </h1>
+        <div className={styles.indexCtas}>
+          <Link className="button button--primary" to="/docs">
+            <Translate>Get Started</Translate>
+          </Link>
+          <Link className="button button--info" to="https://docusaurus.new">
+            <Translate>Try a Demo</Translate>
+          </Link>
+          <span className={styles.indexCtasGitHubButtonWrapper}>
+            <iframe
+              className={styles.indexCtasGitHubButton}
+              src="https://ghbtns.com/github-btn.html?user=facebook&amp;repo=docusaurus&amp;type=star&amp;count=true&amp;size=large"
+              width={160}
+              height={30}
+              title="GitHub Stars"
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+function Feature({
+  feature,
+  className,
+}: {
+  feature: FeatureItem;
+  className?: string;
+}) {
+  const {withBaseUrl} = useBaseUrlUtils();
+
+  return (
+    <div className={clsx('col', className)}>
+      <img
+        className={styles.featureImage}
+        alt={feature.title}
+        width={Math.floor(feature.image.width)}
+        height={Math.floor(feature.image.height)}
+        src={withBaseUrl(feature.image.src)}
+        loading="lazy"
+      />
+      <h3 className={clsx(styles.featureHeading)}>{feature.title}</h3>
+      <p className="padding-horiz--md">{feature.text}</p>
+    </div>
+  );
+}
+
+function FeaturesContainer() {
+  const firstRow = Features.slice(0, 3);
+  const secondRow = Features.slice(3);
+
+  return (
+    <div className="container text--center">
+      <h2>
+        <Translate>Main features</Translate>
+      </h2>
+      <div className="row margin-bottom--lg">
+        {firstRow.map((feature, idx) => (
+          <Feature feature={feature} key={idx} />
+        ))}
+      </div>
+      <div className="row">
+        {secondRow.map((feature, idx) => (
+          <Feature
+            feature={feature}
+            key={idx}
+            className={clsx('col--4', idx === 0 && 'col--offset-2')}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {
+    siteConfig: {customFields, tagline},
+  } = useDocusaurusContext();
+  const {description} = customFields as {description: string};
+  return (
+    <Layout title={tagline} description={description}>
+      <main>
+        <HeroBanner />
+        <div className={styles.section}>
+          <FeaturesContainer />
+        </div>
+      </main>
+    </Layout>
+  );
+}
